c71626d5480e99426d1b6ec4bec29fb2c16dd80f
morgancostigan <morgan.costigan@gmail.com>
2020-09-21 16:50:25 -0500

    worked out handleClick for claring and updating, todo list for running equation

--- src/App.js
+++ src/App.js
@@ -8,13 +8,43 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
+      tempEquation: "",
       equation: "pine forest",
       calculations: []
     }
   }//end constructor
 
+  clearEquation() {
+    this.setState({
+      equation: ""
+    })
+  };//end clearEquation
+
+  updateEquation(buttonName){
+    this.setState({
+      equation: this.state.equation + buttonName
+    })
+  };//end updateEquation
+
+  runEquation(){
+    //1 set as tempEquation
+    //2 evaluate the equation
+    //3 display as "tempEquation" = "equation"
+    //4 save tempEquation = equation as calculation
+    //5 send calculation to a more permanent save (json or db)
+  }
+
   handleClick = buttonName => {
     console.log(buttonName);
+
+    if(buttonName === "C"){
+      this.clearEquation()
+    }
+    else if(buttonName === "="){
+      this.runEquation()
+    } else {
+      this.updateEquation(buttonName)
+    }
   }
 
 
