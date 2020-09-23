0d402393abdef8d78643cd89e76aa342ef55ece9
morgancostigan <morgan.costigan@gmail.com>
2020-09-22 20:36:09 -0500

    finished runEquation function that generates a solution, pass that to setCalc function that pushes the equation and solution into an array

--- src/App.js
+++ src/App.js
@@ -10,38 +10,108 @@ class App extends Component {
   }
 
   state = {
+    answer: "",
     tempEquation: "",
     equation: "pine forest",
+    calculation: "",
     calculations: []
   }
 
   clearEquation() {
     this.setState({
-      equation: ""
+      equation: "",
+      answer: ""
     })
   };//end clearEquation
 
   updateEquation(buttonName){
-    this.setState({
-      equation: this.state.equation + buttonName
-    })
+    if(!this.state.answer){
+      this.setState({
+        equation: this.state.equation + buttonName
+      })
+    } else {
+      this.setState({
+        equation: JSON.stringify(this.state.answer) + buttonName,
+        answer: ""
+      })
+    }
   };//end updateEquation
 
   runEquation(){
-    //1 set as tempEquation
+    let solution = eval(this.state.equation);
     this.setState({
-      tempEquation: this.state.equation,
-    //2 evaluate the equation
-      equation: (eval(this.state.equation))
-    }, () => {
-      console.log("state:", this.state);  
-    });
-    // console.log("state:", this.state);    
+      answer: solution,
+    })
+    this.setCalc(solution)
+    console.log('state:', this.state);
+  }//end runEquation
 
-    //3 display as "tempEquation=equation"
-    //4 save "tempEquation=equation" as calculation
-    //5 send calculation to a more permanent save (json or db)
-  }
+  // componentDidUpdate(prevProps) {
+  //   // Typical usage (don't forget to compare props):
+  //   if (this.state.answer !== prevProps.answer) {
+  //     // this.setState({
+  //     //   calculations: [`${this.state.equation}=${this.state.answer}`].concat(this.state.calculations)
+  //     // })
+  //     console.log("this.state.answer", this.state.answer, "prevState.answer:", prevProps.answer);
+  //   }
+  // }
+
+  setCalc(solution){ 
+    this.setState({
+      calculations: [`${this.state.equation}=${solution}`].concat(this.state.calculations)
+    })
+    console.log('state2:', this.state);
+  }//end setCalc
+
+  // runEquation(){
+  //   //1 set as tempEquation
+  //   this.setState({
+  //     tempEquation: this.state.equation,
+  //   //2 evaluate the equation
+  //     equation: (eval(this.state.equation)),
+  //         //3 display as "tempEquation=equation"
+
+  //   }, () => {
+  //     this.setState({
+  //       calculation: (this.state.tempEquation) = (JSON.stringify(this.state.equation))
+  //     }, () => { console.log("state:", this.state); })
+ 
+  //   });
+  //   //4 save "tempEquation=equation" as calculation
+  //   //5 send calculation to a more permanent save (json or db)
+  // };
+
+  // runEquation(){
+  //   this.setState({
+  //     tempEquation: this.state.equation
+  //   }, () => {
+  //     this.setState({
+  //       equation: (eval(this.state.equation))
+  //     }, () => {
+  //       this.setState({
+  //         calculation: (this.state.tempEquation = JSON.stringify(this.state.equation))
+  //       }, () => {
+  //           console.log("state:", this.state)
+  //       })
+  //     })
+  //   })
+  // }
+
+  // runEquation(){
+  //   this.setState({
+  //     tempEquation: this.state.equation
+  //   }).then(
+  //     this.setState({
+  //       equation: (eval(this.state.equation))
+  //     })
+  //   ).then(
+  //     this.setState({
+  //       calculation: (this.state.tempEquation = JSON.stringify(this.state.equation))
+  //     })
+  //   ).then(
+  //     console.log('state:', this.state)
+  //   )
+  // }
 
   handleClick = buttonName => {
     console.log(buttonName);
@@ -60,7 +130,7 @@ class App extends Component {
     return (
       <div className="App">
         <Calculator onClick={this.handleClick}/>
-        <Equation equation={this.state.equation}/>
+        <Equation equation={this.state.equation} answer={this.state.answer}/>
 
       </div>
     );
--- src/Components/Equation.js
+++ src/Components/Equation.js
@@ -4,11 +4,19 @@ import React, { Component } from 'react'
 class Equation extends Component{
     render(){
         let displayEquation = this.props.equation;
+        let displayAnswer = this.props.answer;
+        let display;
         console.log('props', this.props.equation);
 
+        //this headache right here
+        if(typeof displayAnswer !== "number" ){
+            display = displayEquation
+        } else {
+            display = displayAnswer
+        }
         return (
             <p>
-                {displayEquation}
+                {display}
             </p>
         )
     }
